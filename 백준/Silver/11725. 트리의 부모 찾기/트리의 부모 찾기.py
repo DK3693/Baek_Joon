67064cc0ee0a2bf00430eefbaf452e69ad657611
DK3693 <ehdrb0169@naver.com>
2026-01-08 19:25:59 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 300 ms, Memory: 51724 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,28 +1,26 @@
 import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
+
 input = sys.stdin.readline
 
 N = int(input())
-nodes = [tuple(map(int, input().split())) for _ in range(N - 1)]  # nodes 유지
 
-# 각 노드 -> (nodes에서) 자신이 등장하는 간선 인덱스들
-adj_edge_idx = [[] for _ in range(N + 1)]
-for idx, (a, b) in enumerate(nodes):
-    adj_edge_idx[a].append(idx)
-    adj_edge_idx[b].append(idx)
+graph = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
 parent = [0] * (N + 1)
-parent[1] = -1  # 루트
+parent[1] = -1 
 
-def dfs(cur: int) -> None:
-    for eidx in adj_edge_idx[cur]:
-        a, b = nodes[eidx]
-        nxt = b if a == cur else a  # cur의 반대편 노드
-        if parent[nxt] == 0:        # 방문 체크
-            parent[nxt] = cur
-            dfs(nxt)
-
-dfs(1)
+q = deque([1])
+while q:
+    cur = q.popleft()
+    for nxt in graph[cur]:
+        if parent[nxt] == 0:     
+            parent[nxt] = cur     
+            q.append(nxt)
 
 for i in range(2, N + 1):
     print(parent[i])
