5c32fbae3a8d07a0bb4170ae04779459dda14192
DK3693 <ehdrb0169@naver.com>
2026-05-12 16:26:20 +0900

    [level 3] Title: 이중우선순위큐, Time: 486.09 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,3 +1,37 @@
+import heapq
+
 def solution(operations):
-    answer = []
-    return answer
+    min_heap = []
+    max_heap = []
+    numbers = []
+
+    for operation in operations:
+        command, number = operation.split()
+        number = int(number)
+
+        if command == "I":
+            heapq.heappush(min_heap, number)
+            heapq.heappush(max_heap, -number)
+            numbers.append(number)
+
+        elif command == "D":
+            try:
+                if number == 1:
+                    num = -heapq.heappop(max_heap)
+                    min_heap.remove(num)
+                    numbers.remove(num)
+                    heapq.heapify(min_heap)
+
+                elif number == -1:
+                    num = heapq.heappop(min_heap)
+                    max_heap.remove(-num)
+                    numbers.remove(num)
+                    heapq.heapify(max_heap)
+
+            except:
+                pass
+
+    if len(numbers) == 0:
+        return [0, 0]
+
+    return [max(numbers), min(numbers)]
